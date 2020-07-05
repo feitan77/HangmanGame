04440d8a1731ca405e43dd72d2b18f5bc04f0d2b
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2020-07-05 02:15:50 -0400

    Update ps3_hangman.py

--- ps3_hangman.py
+++ ps3_hangman.py
@@ -11,76 +11,56 @@ def loadWords():
     return wordlist
 
 def chooseWord(wordlist):
-   
     return random.choice(wordlist)
 
 wordlist = loadWords()
 
 def isWordGuessed(secretWord, lettersGuessed):
-    for i in secretWord:
-        if i not in lettersGuessed:
-            return False
-    return True
-
-
+    return not (set(secretWord) - set(lettersGuessed))
 
 
 def getGuessedWord(secretWord, lettersGuessed):
-    
-    result=[]
-    
-    
-    for i in secretWord:
-        if i in lettersGuessed:
-           result.append(i)
-        else:
-            result.append("_")
-    s=''.join(result)
-    return s
+    return ''.join(['_', s][s in lettersGuessed] for s in secretWord)
 
 
 def getAvailableLetters(lettersGuessed):
     import string
-    L=list(string.ascii_lowercase)
-    
-    for i in lettersGuessed:
-        
-        L.remove(i)
-    
-    y=''.join(L)
-    return y
+    lst = [
+        s for s in string.ascii_lowercase if s not in lettersGuessed
+    ]
+    return ''.join(lst)
 
     
 
 def hangman(secretWord):
     
     print('Welcome to the game, Hangman!')
-    print('I am thinking of a word that is', len(secretWord), "letters long.")
+    print(f'I am thinking of a word that is {len(secretWord)} letters long.')
     mistakesMade = 0
     lettersGuessed = []
 
     while 8 - mistakesMade > 0:
-        if isWordGuessed(secretWord, lettersGuessed) == True:
+        if isWordGuessed(secretWord, lettersGuessed):
             print('------------')
             print('Congratulations, you won!')
             break
         else:
         	print('------------')
-        	print('You have', 8 - mistakesMade, 'guesses left.')
-        	print('Available letters:', getAvailableLetters(lettersGuessed))
+        	print(f'You have {8 - mistakesMade} guesses left.')
+        	print(f'Available letters: {getAvailableLetters(lettersGuessed)}')
         	guess = str(input('Please guess a letter:')).lower()
         	if guess in secretWord and guess not in lettersGuessed:
         		lettersGuessed.append(guess)
-        		print('Good guess:', getGuessedWord(secretWord, lettersGuessed))
+        		print(f'Good guess: {getGuessedWord(secretWord, lettersGuessed)}')
         	elif guess in lettersGuessed:
-        		print("Oops! You've already guessed that letter:", getGuessedWord(secretWord, lettersGuessed))
+        		print(f"Oops! You've already guessed that letter: {getGuessedWord(secretWord, lettersGuessed)}")
         	elif guess not in secretWord:
-        		print("Oops! That letter is not in my word:", getGuessedWord(secretWord, lettersGuessed))
+        		print(f"Oops! That letter is not in my word: {getGuessedWord(secretWord, lettersGuessed)}")
         		lettersGuessed.append(guess)
         		mistakesMade += 1
         if 8 - mistakesMade == 0:
         	print('------------')
-        	print('Sorry, you ran out of guesses. The word was', secretWord)
+        	print(f'Sorry, you ran out of guesses. The word was {secretWord}')
         	break
         else:
         	continue
